00f5a944db297a7327114699c47653877d98ca49
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-01-09 17:26:22 +0100

    Lavoro sul bonus

--- routers/postsRouters.js
+++ routers/postsRouters.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const postsController = require (`../controllers/postsController`)
+// const validate = require(`../middlewares/validate`)
 
 router.get(`/`, postsController.index)
 
